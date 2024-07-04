604d7b4e67a182abf8dcdd1750fe74997e8e9f1a
unknown <nrpatel7@gmail.com>
2024-07-04 13:17:01 +0100

    week3-js updateed

--- Week3-js/contact.js
+++ Week3-js/contact.js
@@ -1,1 +1,22 @@
-console.log('button pressed')
+
+function sendContactDetails(){
+
+    const form = document.getElementById('contact-form')
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+
+    const formData =  new FormData(form)
+
+    data = Object.fromEntries(formData.entries)
+
+    console.log("DForm", data)
+
+
+
+
+});
+}
+
+
+
